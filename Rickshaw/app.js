d07f7db7f9d666c6c44d94903613535d9997f92b
Gary Grumbley <g.grumbley@alphasystem.lan>
2017-09-15 14:20:11 -0700

    Add Driver model to Rickshaw

--- Rickshaw/app.js
+++ Rickshaw/app.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-// const routes = require('./routes');
+const routes = require('./routes');
 
 const app = express();
 
 mongoose.Promise = global.Promise;
 if (process.env.NODE_ENV !== 'test') {
-  mongoose.connect('mongodb://localhost/rickshaw');
+  mongoose.createConnection('mongodb://localhost/rickshaw');
 }
 
 app.use(bodyParser.json());
-// routes(app);
+routes(app);
 
 app.use((err, req, res, next) => {
   res.status(422).send({ error: err.message });
